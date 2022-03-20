bf95b2575d4f8e514321978dea13bf7343cdf8f7
Daniel Fischer <danielfm123@gmail.com>
2022-03-20 17:24:11 -0300

    usd

--- loop.py
+++ loop.py
@@ -1,8 +1,16 @@
 import uf
+import usdclp
 
-for a in range(1990,2020):
-    print(a)
-    uf.scrap(a)
+for anio in range(1990,2023):
+    print(anio)
+    usdclp.scrap(anio)
+
+anio = 2002
+uf.scrap(anio)
+
+#tabla = "dfischer.valor_serie"
+#con = db_geekosas("dfischer")
 
 
-tabla = "dfischer.valor_serie"
+a = [1,2,3,4,5,6,7]
+a[:-1]
